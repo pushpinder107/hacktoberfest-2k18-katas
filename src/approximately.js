afeb450ac3c0d591f56847a77d4f8109a4f81776
Pushpinder <pushpinder107@gmail.com>
2018-10-14 16:23:53 +0530

    Add implementation for approximately.js

--- src/approximately.js
+++ src/approximately.js
@@ -1,3 +1,9 @@
-export const approximately = margin => (lhs, rhs) => {
-  // TO BE IMPLEMENTED IN ANOTHER PR
+export const approximately = margin => {
+   if (typeof margin !== 'number' || margin < 0 || margin === undefined || Number.isNaN(margin)) {
+     throw 'Incorrect value for margin';
+   }
+  return (lhs, rhs) => {
+   return Math.abs(lhs - rhs) <= margin;
+ }
 };
+
